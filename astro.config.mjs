a1bfaa6859148cb167d746e7e0be69a73c4510b1
Ahmed Chaabni <chaabni.dev@gmail.com>
2026-03-10 19:33:45 +0100

    Upgrade to Astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -122,7 +122,7 @@ export default defineConfig({
   site: process.env.SITE_URL || 'https://cooper.gladtek.com',
   output: 'static',
   image: {
-    domains: ['vitejs.dev', 'upload.wikimedia.org', 'astro.build', 'pagepro.co'],
+    domains: ['vite.dev', 'vitejs.dev', 'upload.wikimedia.org', 'astro.build', 'pagepro.co'],
   },
   adapter: getAdapter(),
   integrations: [
